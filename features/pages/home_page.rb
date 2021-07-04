8340b8d0f41a3d847999c46a93ca19bd3c3db705
Cristine Costa <cristine.cardoso@cilia.com.br>
2021-07-04 20:28:05 -0300

    completando os localizadores da homepage

--- features/pages/home_page.rb
+++ features/pages/home_page.rb
@@ -2,13 +2,13 @@ module Pages
   class HomePage < SitePrism::Page
     set_url '/index.php'
 
-    element  :logo, ''
-    element  :btn_contact_us, ''
-    element  :btn_go_to_sign_in, ''
+    element  :logo, '#header-logo'
+    element  :btn_contact_us, '#contact-link'
+    element  :btn_go_to_sign_in, '.login'
     element  :input_search_form, '#search_query_top'
     element  :button_search, '.button-search'
-    element  :input_newsletter_footer_email, ''
-    element  :btn_newsletter_footer_register, ''
+    element  :input_newsletter_footer_email, '#newsletter-input'
+    element  :btn_newsletter_footer_register, '.submitNewsletter'
 
     def search(query)
       input_search_form.set(query)
